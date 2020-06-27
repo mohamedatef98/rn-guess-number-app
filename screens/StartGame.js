effdd9e63806b1e4793f7d8eaca6b87a19625800
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-06-27 05:43:38 +0200

    Add Controls to StartGame Screen Component

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native'
 
 const StartGame = props => {
     return <View style={styles.screen}>
         <Text>Game Screen</Text>
-    </View>
+        <View style={styles.inputContainer}>
+            <Text style={styles.title}>Start new Game</Text>
+            <TextInput />
+            <View style={styles.buttonContainer}>
+                <Button title="Reset" />
+                <Button title="Confirm" />
+            </View>
+        </View>
+    </View> 
 }
 
 const styles = StyleSheet.create({
@@ -12,6 +20,21 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         padding: 10
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 })
 
