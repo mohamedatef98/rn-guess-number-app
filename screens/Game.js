a96e5edc8320edafa7426b439e880ad041bf254d
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-06-29 01:23:06 +0200

    Add Game Screen

--- screens/Game.js
+++ screens/Game.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+import { NumberContainer, Card } from '../components'
 
 const generateRandomNumber = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -14,9 +16,29 @@ const generateRandomNumber = (min, max, exclude) => {
 const Game = ({ userChoice }) => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, userChoice))
     return (
-        <View>
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" />
+                <Button title="GREATER" />
+            </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        alignItems: 'center',
+        padding: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
+})
 
 export default Game
