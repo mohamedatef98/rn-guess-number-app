3cc6bcd5c8934088c04ba8b6e7b7a24c91b4e017
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-07-01 06:52:44 +0200

    Add Number guessing to Game Screen

--- screens/Game.js
+++ screens/Game.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, Text, StyleSheet, Button, Alert } from 'react-native'
 
 import { NumberContainer, Card } from '../components'
+
+const DIRECTION_LOWER = 'lower'
+const DIRECTION_GREATER = 'greater'
 
 const generateRandomNumber = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -15,13 +18,36 @@ const generateRandomNumber = (min, max, exclude) => {
 
 const Game = ({ userChoice }) => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, userChoice))
+
+    const currentLow = useRef(1)
+    const currentHigh = useRef(100)
+
+    const nextGuessHandler = direction => {
+            if ((direction === DIRECTION_LOWER && currentGuess < userChoice) || (direction === DIRECTION_GREATER && currentGuess > userChoice))
+                return Alert.alert('Don\'t lie!', 'You know that was wrong...', [{ text: 'Sorry!', style: 'cancel' }])
+
+            else if (direction === DIRECTION_LOWER)
+                currentHigh.current = currentGuess
+
+            else if (direction === DIRECTION_GREATER)
+                currentLow.current = currentGuess
+
+            const nextRnd = generateRandomNumber(currentLow.current, currentHigh.current, currentGuess)
+
+            setCurrentGuess(nextRnd)
+        }
+    
+    const lowerPressHandler = () => nextGuessHandler(DIRECTION_LOWER)
+
+    const greaterPressHandler = () => nextGuessHandler(DIRECTION_GREATER)
+
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" />
-                <Button title="GREATER" />
+                <Button title="LOWER" onPress={lowerPressHandler} />
+                <Button title="GREATER" onPress={greaterPressHandler} />
             </Card>
         </View>
     )
