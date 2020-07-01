b5d1992acd487e8e28004fe9c3db9268dcb67881
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-07-01 12:48:05 +0200

    Add GameOver Image

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, Image } from 'react-native'
 
 const GameOver = ({ numOfGuesses, userChoice, onRestart }) => {
     return <View style={styles.screen}>
-        <Text>Game is Over!</Text>
+        <Text style={styles.title}>Game is Over!</Text>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require('../assets/success.png')} />
+        </View>
         <Text>Your Number was {userChoice}</Text>
         <Text>It took {numOfGuesses} guesses to find it</Text>
         <Button title="New Game ?" onPress={onRestart} />
@@ -15,6 +18,23 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderColor: 'black',
+        borderWidth: 3,
+        borderRadius: 150,
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 })
 
