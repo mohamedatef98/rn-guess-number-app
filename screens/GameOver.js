c70045d0892614b3784d11bf613c24ffca3db203
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-07-22 14:12:48 +0200

    Improve GameOver Screen Responsive Style

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,9 +1,44 @@
 import React from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Image, ScrollView } from 'react-native'
 import { Colors, Fonts } from '../theme'
 import { Button, Text } from '../components'
+import { useStyles } from '../hooks'
 
 const GameOver = ({ numOfGuesses, userChoice, onRestart }) => {
+    const styles = useStyles(({ dimensions }) => ({
+        screen: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1
+        },
+        title: {
+            fontFamily: Fonts.bold,
+            fontSize: dimensions.height / 20
+        },
+        imageContainer: {
+            width: dimensions.height / 3,
+            height: dimensions.height / 3,
+            borderColor: 'black',
+            borderWidth: 3,
+            borderRadius: dimensions.height / 6,
+            overflow: 'hidden',
+            marginVertical: dimensions.height / 25
+        },
+        image: {
+            width: '100%',
+            height: '100%'
+        },
+        summary: {
+            fontSize: dimensions.height / 25,
+            marginVertical: dimensions.height / 100,
+            marginHorizontal: dimensions.width / 20
+        },
+        highlighted: {
+            color: Colors.primary,
+            fontFamily: Fonts.bold
+        }
+    }))
+
     return <View style={styles.screen}>
         <Text style={styles.title}>Game is Over!</Text>
         <View style={styles.imageContainer}>
@@ -13,39 +48,5 @@ const GameOver = ({ numOfGuesses, userChoice, onRestart }) => {
         <Button onPress={onRestart}>New Game ?</Button>
     </View>
 }
-
-const styles = StyleSheet.create({
-    screen: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1
-    },
-    title: {
-        fontFamily: Fonts.bold,
-        fontSize: 20
-    },
-    imageContainer: {
-        width: 300,
-        height: 300,
-        borderColor: 'black',
-        borderWidth: 3,
-        borderRadius: 150,
-        overflow: 'hidden',
-        marginVertical: 30
-    },
-    image: {
-        width: '100%',
-        height: '100%'
-    },
-    summary: {
-        fontSize: 20,
-        marginVertical: 30,
-        marginHorizontal: 15
-    },
-    highlighted: {
-        color: Colors.primary,
-        fontFamily: Fonts.bold
-    }
-})
 
 export default GameOver
