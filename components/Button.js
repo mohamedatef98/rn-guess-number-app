2be08d57de8c45a749138542b9ca2799efe04764
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-07-22 15:10:13 +0200

    Use TouchableNativeFeedback in Button Component on Android

--- components/Button.js
+++ components/Button.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useMemo } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Platform, TouchableNativeFeedback } from 'react-native'
 import { Colors, Fonts } from '../theme'
 
-const Button = ({ children, style, onPress, activeOpacity=0.8 }) => (
-    <TouchableOpacity onPress={onPress} activeOpacity={activeOpacity} >
-        <View style={[styles.button, style]}>
-            <Text style={styles.text}>{children}</Text>
-        </View>
-    </TouchableOpacity>
-)
+const Button = ({ children, style, onPress, activeOpacity = 0.8 }) => {
+    const commonTouchableProps = {
+        onPress
+    }
+
+    const Touchable = Platform.OS === 'android' ?
+        ({ children }) => <TouchableNativeFeedback {...commonTouchableProps}>
+            {children}
+        </TouchableNativeFeedback> :
+        ({ children }) => <TouchableOpacity {...commonTouchableProps} activeOpacity={activeOpacity}>
+            {children}
+        </TouchableOpacity>
+
+    return <View style={styles.buttonContainer}>
+        <Touchable>
+            <View style={[styles.button, style]}>
+                <Text style={styles.text}>{children}</Text>
+            </View>
+        </Touchable>
+    </View>
+}
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 25,
+        overflow: 'hidden'
+    },
     button: {
         backgroundColor: Colors.primary,
         paddingVertical: 12,
-        paddingHorizontal: 30,
-        borderRadius: 25
+        paddingHorizontal: 30
     },
     text: {
         color: 'white',
